df44f563fae5f2e79c6129586e3a931362c360d6
kostya-wolf <kostya-wolf@yandex.ru>
2021-11-03 11:27:16 +0300

    HW02 4:Реализованы слои приложения, 5:классы включены в контекст Spring (добавлены аннотации)

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,20 +1,28 @@
 package ru.javawebinar.topjava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
 public class SpringMain {
+    private static final Logger logger = LoggerFactory.getLogger(SpringMain.class);
+
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+
+            MealRestController mealRestController = appCtx.getBean(MealRestController.class);
+            logger.debug(mealRestController.getAll().toString());
         }
     }
 }

d97135985d4399e8ff615b377eff7c226ebf9053
kostya-wolf <kostya-wolf@yandex.ru>
2021-11-05 14:06:49 +0300

    HW03 Дополнены скрипты создания и инициализации таблицы БД MEALS; Реализован через Spring JDBC Template JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES (TIMESTAMP '2020.01.30 10:00', 'Завтрак', 500, 100000),
+       (TIMESTAMP '2020.01.30 12:00', 'Обед', 500, 100000),
+       (TIMESTAMP '2020.01.30 20:00', 'Ужин', 500, 100000),
+       (TIMESTAMP '2021.05.11 20:00', 'Ужин', 500, 100001),
+       (TIMESTAMP '2021.05.11 22:00', 'Ням-ням', 500, 100001);

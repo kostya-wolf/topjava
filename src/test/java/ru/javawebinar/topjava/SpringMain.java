825daab179032e71decaecba45b7dae5fbcfbc5f
kostya-wolf <kostya-wolf@yandex.ru>
2023-03-20 18:14:03 +0300

    HW05 Optional 5: В SpringMain добавил профили. Поднял Spring контекст без использования spring.profiles.active

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealTo;
@@ -15,9 +15,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SpringMain {
+    private static final String INMEMORY_PROFILE = "spring/inmemory.xml";
+    private static final String DB_PROFILE = "spring/spring-db.xml";
+    private static final String ACTIVE_SPRING_PROFILE = DB_PROFILE;
+
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
+        try (GenericXmlApplicationContext appCtx = new GenericXmlApplicationContext()) {
+            ConfigurableEnvironment env = appCtx.getEnvironment();
+            env.setActiveProfiles(Profiles.getActiveDbProfile(), Profiles.REPOSITORY_IMPLEMENTATION);
+            appCtx.load("spring/spring-app.xml", ACTIVE_SPRING_PROFILE);
+            appCtx.refresh();
+
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
